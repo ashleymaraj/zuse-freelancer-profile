1eb5faeda10aa7a7b25bccdc6cbd21aa265e3c6f
ashleymaraj <ashley@zusesystems.com>
2024-10-11 09:06:05 +0200

    commit

--- zuse-freelancer-profile/src/app/services/profile.service.ts
+++ zuse-freelancer-profile/src/app/services/profile.service.ts
@@ -9,6 +9,7 @@ export interface Profile {
   bio: string;
   skills: string[];
   portfolio: string[];
+  portfolioBio: string;
   socialMedia: Record<string, string>;
   contact: { email: string; phone: string };
 }
@@ -25,7 +26,8 @@ export class ProfileService {
       hourlyRate: 450,
       bio: '20 years experience in Angular development',
       skills: ['Angular', 'JavaScript', 'CSS', 'HTML'],
-      portfolio: ['assets/image1.jpg', 'assets/image2.jpg'],
+      portfolio: ['assets/images/mtn.png', 'assets/images/vodacom.png', 'assets/images/stdbank.jpg', 'assets/images/fnb.png', 'assets/images/absa.png'],
+      portfolioBio: 'The below links will take you the work I have done for these companies',
       socialMedia: {
         LinkedIn: 'https://linkedin.com/in/walterwhite',
         X: 'https://x.com/walterwhite',
